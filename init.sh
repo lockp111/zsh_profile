c67b91a8a7ae67ca6dd475a98444bd693159fdbb
harman <lockp111@icloud.com>
2018-09-18 14:50:05 +0800

    refact: init.sh

--- init.sh
+++ init.sh
@@ -2,12 +2,13 @@
 brew install zsh
 brew cask install iterm2
 
-git clone https://github.com/powerline/fonts.git
+git clone https://github.com/powerline/fonts.git ./fonts
 cd fonts && sh ./install.sh
 rm -rf fonts
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-chsh -s /bin/zsh
+sudo hsh -s /bin/zsh
+
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 cp zsh_profile ~/.zshrc
-cp -r ./themes ~/.oh-my-zsh/custom/
+cp -rf ./themes ~/.oh-my-zsh/custom/
