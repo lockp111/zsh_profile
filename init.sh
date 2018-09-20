370eef32a3a0d1be5e657819bdb79b32b9243ec8
harman <lockp111@icloud.com>
2018-09-20 22:55:14 +0800

    feat: fzf

--- init.sh
+++ init.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 brew install zsh
 brew cask install iterm2
+brew install fzf
 
 git clone https://github.com/powerline/fonts.git ./fonts
 cd fonts && sh ./install.sh
 rm -rf fonts
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-sudo hsh -s /bin/zsh
+sudo chsh -s /bin/zsh
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 cp zsh_profile ~/.zshrc
 cp -rf ./themes ~/.oh-my-zsh/custom/
